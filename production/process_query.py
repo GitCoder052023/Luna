afb789d1cabe0bcd9403669e5a7de6ddc4449cf7
GitCoder052023 <K.alam93899@gmail.com>
2024-03-07 14:38:05 +0530

    moving files

--- production/process_query.py
+++ production/process_query.py
@@ -7,7 +7,7 @@ def query_processer(query_source):
     while True:
         query = query_source
 
-        genai.configure(api_key="REPLACE WITH YOUR OWN API KEY")
+        genai.configure(api_key="AIzaSyBoxqElIKXzz7IxkiEllxyDQlYbasCkbTc")
 
         model = genai.GenerativeModel('gemini-pro')
         print("Generating content for you...")
